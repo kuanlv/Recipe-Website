5770301cb881967904799036fc518168ee0f2b4a
kuan lyu <46909761+kuanlv@users.noreply.github.com>
2020-06-17 23:54:51 -0400

    add step

--- recipe-app/src/app/recipe-form/recipe-form.component.ts
+++ recipe-app/src/app/recipe-form/recipe-form.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Renderer2, ViewChild, TemplateRef, ViewContainerRef } from '@angular/core';
 import { NgForm } from '@angular/forms';
 
 @Component({
@@ -8,15 +8,42 @@ import { NgForm } from '@angular/forms';
 })
 
 export class RecipeFormComponent implements OnInit {
+   @ViewChild("stepInputs", { static: true }) stepInputs;
+   @ViewChild("stepTitle", { static: true }) stepTitle;
 
-   constructor() { }
+   steps: Array<any> = new Array();
+   noOfStep: number = 0;
+   step: {
+      stepId: number,
+      stepTitle: string,
+      stepDescription: string,
+      stepUrl: string,
+      stepImg: string
+   } = {
+         stepId: 0,
+         stepTitle: "",
+         stepDescription: "",
+         stepUrl: "",
+         stepImg: ""
+      };
 
-   ngOnInit(): void {
+   constructor(private renderer: Renderer2) {
 
    }
 
+   ngOnInit(): void {
+   }
+
+   copyStep() {
+      const stepCopy = JSON.parse(JSON.stringify(this.step));
+      this.steps.push(stepCopy);
+      console.log();
+      this.noOfStep += 1;
+      this.step.stepId = this.noOfStep;
+   }
+
    onSubmit(form: NgForm) {
-      console.log(form);
+      this.renderer
    }
 
 }
